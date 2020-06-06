adf3a5978b32cf0b18f4f6671bfd3c0519b6fb6f
Jacek <jaceksysiak@wp.pl>
2020-06-06 09:15:14 +0200

    mmr

--- src/main/java/pl/jaceksysiak/ecommerce/dao/ProductRepository.java
+++ src/main/java/pl/jaceksysiak/ecommerce/dao/ProductRepository.java
@@ -1,6 +1,5 @@
 package pl.jaceksysiak.ecommerce.dao;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
