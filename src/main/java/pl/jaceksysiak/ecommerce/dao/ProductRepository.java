3b34dde64deac2101035f03af3840175e4b28547
Jacek <jaceksysiak@wp.pl>
2020-06-06 08:42:02 +0200

    mmr

--- src/main/java/pl/jaceksysiak/ecommerce/dao/ProductRepository.java
+++ src/main/java/pl/jaceksysiak/ecommerce/dao/ProductRepository.java
@@ -6,6 +6,9 @@ import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import pl.jaceksysiak.ecommerce.entity.Product;
 
+
+
+
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
